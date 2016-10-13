4a85e36b3587a98229d36ac3d7205d1d7b2606eb
sachitjani81 <sachitjani81@gmail.com>
2016-10-13 23:28:16 +0530

    Add files via upload

---
+++ kmtomiles.py
@@ -0,0 +1,11 @@
+""" Program to convert kilometers into miles. """
+
+""" change this value for a different result. """
+kilometers = 5.5
+
+""" conversion factor. """
+conv_fac = 0.621371
+
+""" calculate miles. """
+miles = kilometers * conv_fac
+print('%0.3f kilometers is equal to %0.3f miles' %(kilometers,miles))
